d345bff7ef970b33dc279823cbda210e16645376
Magenta17 <152224120+Magenta17@users.noreply.github.com>
2024-08-16 00:38:09 +0300

    added navbar and fotter

---
+++ lap-1/src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            {  <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>}
+        </nav>
+          
+    );
+}
+
+export default Navbar;
